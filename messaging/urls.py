fc95a85097fc454160ed9a855396857399fbb1dc
Santi <santiago.imoberdoff@gmail.com>
2026-04-01 20:24:22 -0300

    Add messaging app, admin registration and superuser

--- messaging/urls.py
+++ messaging/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.inbox, name='inbox'),
+    path('send/', views.send_message, name='send_message'),
 ]
--- messaging/views.py
+++ messaging/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import Message
 
-# Create your views here.
+@login_required
+def inbox(request):
+    messages_received = Message.objects.filter(receiver=request.user)
+    messages_sent = Message.objects.filter(sender=request.user)
+    return render(request, 'messaging/inbox.html', {
+        'messages_received': messages_received,
+        'messages_sent': messages_sent,
+    })
+
+@login_required
+def send_message(request):
+    users = User.objects.exclude(id=request.user.id)
+    receiver_id = request.GET.get('receiver')
+    if request.method == 'POST':
+        receiver_id = request.POST['receiver']
+        content = request.POST['content']
+        receiver = get_object_or_404(User, id=receiver_id)
+        Message.objects.create(sender=request.user, receiver=receiver, content=content)
+        return redirect('inbox')
+    return render(request, 'messaging/message_form.html', {
+        'users': users,
+        'receiver_id': receiver_id,
+    })
