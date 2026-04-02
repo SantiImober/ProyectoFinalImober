71e2842e50c13131bd685898376a10d37d35fbdd
Santi <santiago.imoberdoff@gmail.com>
2026-04-01 21:06:51 -0300

    Add search functionality

--- blog/views.py
+++ blog/views.py
@@ -22,6 +22,16 @@ class PostListView(ListView):
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
 
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query:
+            return Post.objects.filter(titulo__icontains=query)
+        return Post.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['query'] = self.request.GET.get('q', '')
+        return context
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
